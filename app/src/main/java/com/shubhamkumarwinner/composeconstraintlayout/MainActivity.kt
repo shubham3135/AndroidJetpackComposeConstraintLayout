93f75cc574f5d28f9ed292f69d08fea7c3eaff5f
Shubham <shubhamkumarwinner@gmail.com>
2021-08-10 17:04:54 +0530

    BoxWithConstraints

--- app/src/main/java/com/shubhamkumarwinner/composeconstraintlayout/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composeconstraintlayout/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.shubhamkumarwinner.composeconstraintlayout.ui.theme.ComposeConstraintLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,9 +25,45 @@ class MainActivity : ComponentActivity() {
             ComposeConstraintLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BoxWithConstraintsDemo()
                 }
             }
+        }
+    }
+}
+
+//boxWithConstraints
+@Composable
+fun BoxWithConstraintsDemo() {
+    Column {
+        Column {
+            MyBoxWithConstraintsDemo()
+        }
+
+        Text(
+            "Here we set the size to 150.dp",
+            modifier = Modifier.padding(top = 20.dp)
+        )
+        Column(modifier = Modifier.size(150.dp)) {
+            MyBoxWithConstraintsDemo()
+        }
+    }
+}
+
+@Composable
+private fun MyBoxWithConstraintsDemo() {
+    BoxWithConstraints {
+        val boxWithConstraintsScope = this
+        //You can use this scope to get the minWidth, maxWidth, minHeight, maxHeight in dp and constraints
+
+        Column {
+            if (boxWithConstraintsScope.maxHeight >= 500.dp) {
+                Text(
+                    "This is only visible when the maxHeight is >= 500.dp",
+                    style = TextStyle(fontSize = 20.sp)
+                )
+            }
+            Text("minHeight: ${boxWithConstraintsScope.minHeight}, maxHeight: ${boxWithConstraintsScope.maxHeight},  minWidth: ${boxWithConstraintsScope.minWidth} maxWidth: ${boxWithConstraintsScope.maxWidth}")
         }
     }
 }
@@ -85,6 +121,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeConstraintLayoutTheme {
-        ConstraintLayoutDemo()
+        BoxWithConstraintsDemo()
     }
 }
